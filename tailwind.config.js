c0e53b22a0103444b9bba239675973731f1ef587
giadaf-boosha <giada@boosha.it>
2025-05-28 23:38:34 +0200

    Refactor: Adjust Tailwind content configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   prefix: "",
   theme: {
